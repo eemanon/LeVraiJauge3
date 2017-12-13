f3e96a07defe99a587712894a88b9e10c8c13d6e
louise.gentillet <louise.gentillet@MI000-GS037-08.mathinfo.i-univ-tlse2.fr>
2017-12-13 17:42:36 +0100

    Avec l'implementation des classes

--- src/tec/Autobus.java
+++ src/tec/Autobus.java
@@ -1,5 +1,95 @@
 package tec;
 
-public class Autobus {
+public class Autobus implements Passager, Bus{
+
+	@Override
+	public boolean aPlaceAssise() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean aPlaceDebout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void demanderPlaceAssise(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderPlaceDebout(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderChangerEnDebout(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderChangerEnAssis(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderSortie(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean estDehors() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estAssis() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estDebout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void accepterSortie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceAssise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceDebout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nouvelArret(Bus bus, int numeroArret) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/tec/PassagerStandard.java
+++ src/tec/PassagerStandard.java
@@ -1,5 +1,59 @@
 package tec;
 
-public class PassagerStandard {
+public class PassagerStandard implements Usager, Passager {
+
+	@Override
+	public boolean estDehors() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estAssis() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean estDebout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void accepterSortie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceAssise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accepterPlaceDebout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nouvelArret(Bus bus, int numeroArret) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void monterDans(Transport t) throws UsagerInvalideException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
